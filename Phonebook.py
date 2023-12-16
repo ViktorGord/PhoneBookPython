ce5c3c60973b3fa2cc87af338b686f2b654ce93d
Viktor Gordikov <flaiho@bk.ru>
2023-12-16 17:56:34 +0300

    описаны команды new, search, show

--- Phonebook.py
+++ Phonebook.py
@@ -2,9 +2,30 @@ phonebook = {}
 import json
 print("Телефонный справочник. Краткое руководство\n" #будут перечислены команды, которые повзовляют рабоать со справочником
      "1. show (вывести телефонный справочник);\n"
-     "2. search (поиск по справочнику);\n"
-     "3. new (добавить новую запись в справочник);\n"
+     "2. new (добавить новую запись в справочник);\n"
+     "3. search (поиск по справочнику);\n"
      "4. change (скорректировать запись в справочнике);\n"
      "5. del (удалить запись из справочника);\n"
      "6. export (выгрузить справочник во внешний файл).\n")
-    
+while True:
+    command = input("Введите команду: ")
+    if command =="show": 
+        if len(phonebook) == 0:
+            print("В телефонном справочнике не содержится записей.")
+        else:
+            for surname, values in phonebook.items():
+                print(name, values)
+    elif command =="new":
+        name = input("Введите имя абонента: ")
+        surname = input("Введите фамилию абонента: ")
+        phonenum = input(f"Введите телефон {surname} {name}: ")
+        person_info = {'surname': {surname}, 'name': {name}, 'phonenumber': {phonenum}}
+        phonebook[surname] = person_info 
+        print()
+    elif command =="search":
+        caller = input("Введите фамилию абонента: ") 
+        if caller in phonebook:
+                print(phonebook[caller])
+        else:
+             print(f"Абонент {caller} в справочнике не найден")
+else: print("Такой команды не существует. Введите команду из краткого руководства")
